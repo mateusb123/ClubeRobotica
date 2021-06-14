c0f705e2d54809d0182f859148d527c8c60dc4f4
mateuzin <pg9779628@gmail.com>
2021-06-14 00:13:41 -0300

    ...

--- src/app/routes/admin.js
+++ src/app/routes/admin.js
@@ -1,4 +1,11 @@
 module.exports = function(app) {
+    app.post('/admin', (req,res) => {
+        if(req.body.user == 'Mateus000robotica' && req.body.password == 'Mateus000') {
+            res.render('admin/index.html');
+        } else {
+            res.render('admin/login.html');    
+        }
+    });
     app.use('/admin', (req,res) => {
         res.render('admin/login.html');
     });
