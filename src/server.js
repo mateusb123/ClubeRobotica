201ad5926236c8e464d3fa38227c7908c992c617
mateuzin <pg9779628@gmail.com>
2021-06-14 07:44:24 -0300

    ...

--- src/server.js
+++ src/server.js
@@ -14,6 +14,12 @@ app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'app/public')));
 app.set('views', path.join(__dirname, 'app/views'));
 
+var numberUser = 0;
+io.on('connection', (socket) => {
+    numberUser += 1
+    console.log(`New User connected : ${numberUser}`)
+});
+
 const routerHome = require('./app/routes/home');
 const routerAdmin = require('./app/routes/admin');
 
